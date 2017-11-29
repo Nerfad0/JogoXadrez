212d195c81d686ae9b3a46bb833b8c04e0bf5c7f
Bruno Mundim <brunomundim3232@gmail.com>
2017-11-29 10:19:44 -0200

    Comecando a implementar o tabuleiro

--- Projeto_Parte01/Program.cs
+++ Projeto_Parte01/Program.cs
@@ -1,24 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using tabuleiro;
 
-namespace Projeto_Parte01
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace Projeto_Parte01 {
+    class Program {
+        static void Main(string[] args) { 
 
-            Posicao P;
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
-            P = new Posicao(3, 4);
-
-            Console.WriteLine("Posicao: " + P);
-
-            Console.ReadLine();
+            Console.ReadLine(); 
         }
     }
 }
